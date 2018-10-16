90a1c8b3dada6047423775e6dff604172af50b84
mzmudziak <michal.zmudziak@ibm.com>
2018-10-16 08:36:08 +0200

    Sprawdz czy dwa inty mają odwrotne znaki

--- src/com/umcs/Main.java
+++ src/com/umcs/Main.java
@@ -1,16 +1,32 @@
 package com.umcs;
 
+import java.util.Scanner;
+
 /**
  * 1101
  */
 public class Main {
     public static void main(String[] args) {
-      double nr = 5.2;
-        System.out.println(nr++);
-        System.out.println(nr);
-        System.out.println(++nr);
-        System.out.println(nr);
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Podaj X:");
+            Integer x = input.nextInt();
+            System.out.println("Podaj Y:");
+            Integer y = input.nextInt();
 
+            if(oppositeSign(x, y)) {
+                System.out.println("Różne znaki!");
+            } else {
+                System.out.println("Takie same znaki!");
+            }
+        }
+    }
+
+    public static Boolean oppositeSign(Integer x, Integer y) {
+        System.out.println("X: " + Integer.toBinaryString(x));
+        System.out.println("Y: " + Integer.toBinaryString(y));
+        System.out.println("^: " + (x ^ y));
+        return (x ^ y) < 0;
     }
 }
 
